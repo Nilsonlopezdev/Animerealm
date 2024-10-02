6133c0f8dbfa1faae54c068a77b16de492200d2f
Nilson David Lopez Ospina <devlopez2122@gmail.com>
2024-10-02 22:36:35 -0100

    creacion pagina principal y demas archivos

--- animerealm/src/server.js
+++ animerealm/src/server.js
@@ -12,3 +12,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.listen(3000, () => {
     console.log('Servidor corriendo en http://localhost:3000');
 });
+
+app.get("/", (req, res)=> res.sendFile(__dirname + "/public/contenidos/login,html"))
+app.get("/", (req, res)=> res.sendFile(__dirname + "/public/contenidos/registro.html"))
